09697510e49d9be40ae57df28dad8fb4415ec642
Mare82 <marko.rockyland@gmail.com>
2025-10-04 17:08:44 +0200

    chore: add EAS projectId

--- app.config.js
+++ app.config.js
@@ -6,10 +6,12 @@ export default {
     scheme: "com.mare82.aisanovnik",
     orientation: "portrait",
     version: "1.0.0",
-    plugins: [],
     extra: {
       SUPABASE_URL: process.env.SUPABASE_URL,
-      SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY
+      SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+      eas: {
+        projectId: "e5abc1e7-6bb5-4c80-bb82-42c30f17d129" // <-- ovo iz poruke
+      }
     }
   }
 };
